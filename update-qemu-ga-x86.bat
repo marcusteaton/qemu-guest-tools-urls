3b7b3f23420fec4ffcd162739197186d50cafb16
Marcus Eaton <marcus.eaton@crisisresponseunit.com>
2015-06-23 22:47:37 -0700

    Fixed incorrect -O path

--- update-qemu-ga-x86.bat
+++ update-qemu-ga-x86.bat
@@ -1,1 +1,1 @@
-bin\wget.exe -O .\x86\ https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-qemu-ga/qemu-ga-x86.msi
+bin\wget.exe -O x86\ https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-qemu-ga/qemu-ga-x86.msi
