a30661d0e465d481f0b40c51faa9f9962f6d7c6e
Marcus Eaton <marcus.eaton@crisisresponseunit.com>
2015-06-23 23:17:57 -0700

    Added exit command to auto-close window

--- update-spice-tools.bat
+++ update-spice-tools.bat
@@ -3,3 +3,4 @@ echo.
 bin\wget.exe -q --no-check-certificate http://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-0.100.exe
 echo I- Moving spice-guest-tools-0.100.exe to multiarch folder
 move spice-guest-tools-0.100.exe multiarch
+exit
