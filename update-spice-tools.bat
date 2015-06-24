c939a2033967fae5a3f0bf16c2964284f977b1da
Marcus Eaton <marcus.eaton@crisisresponseunit.com>
2015-06-23 22:47:15 -0700

    Fixed incorrect -O path

--- update-spice-tools.bat
+++ update-spice-tools.bat
@@ -1,1 +1,1 @@
-bin\wget.exe -O .\multiarch\ http://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-0.100.exe
+bin\wget.exe -O multiarch\ http://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-0.100.exe
