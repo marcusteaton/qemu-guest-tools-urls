2f3ef45562359df679d07290e40f8f16bf20fa6b
Marcus Eaton <marcus.eaton@crisisresponseunit.com>
2015-06-23 23:11:01 -0700

    See description
    +: -O option in wget.exe is broken. File must be manually moved with second command
    +: Set wget to no console output (it puts a lot of output to console and looks messy)
    +: Added --no-check-certificate to work around invalid HTTPS cert on download site
    +: Added extra echos to comply with rest of batch file

--- update-qemu-ga-x86.bat
+++ update-qemu-ga-x86.bat
@@ -1,1 +1,5 @@
-bin\wget.exe -O x86\ https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-qemu-ga/qemu-ga-x86.msi
+echo I- Downloading qemu-ga-x86.msi - 1MB
+echo.
+bin\wget.exe -q --no-check-certificate https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-qemu-ga/qemu-ga-x86.msi
+echo I- Moving qemu-ga-x86.msi to x86 folder
+move qemu-ga-x86.msi x86
