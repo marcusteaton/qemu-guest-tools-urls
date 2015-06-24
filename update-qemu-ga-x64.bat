a7173057a6094b08ae78a187900ed409965bdc25
Marcus Eaton <marcus.eaton@crisisresponseunit.com>
2015-06-23 23:17:40 -0700

    Added exit command to auto-close window

--- update-qemu-ga-x64.bat
+++ update-qemu-ga-x64.bat
@@ -3,3 +3,4 @@ echo.
 bin\wget.exe -q --no-check-certificate https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-qemu-ga/qemu-ga-x64.msi
 echo I- Moving qemu-ga-x64.msi to amd64 folder
 move qemu-ga-x64.msi amd64
+exit
