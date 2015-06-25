de8b69d1520e3e20e858d1543852797820937187
Marcus Eaton <marcus.eaton@crisisresponseunit.com>
2015-06-25 11:01:14 -0700

    Added > NUL to clean up output

--- update-qemu-ga-x64.bat
+++ update-qemu-ga-x64.bat
@@ -1,5 +1,5 @@
 echo I- Downloading qemu-ga-x64.msi - 1MB
 echo.
-bin\wget.exe -q --no-check-certificate https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-qemu-ga/qemu-ga-x64.msi
+bin\wget.exe -q --no-check-certificate https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-qemu-ga/qemu-ga-x64.msi > NUL
 echo I- Moving qemu-ga-x64.msi to amd64 folder
 move qemu-ga-x64.msi amd64
