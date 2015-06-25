bd41accdb1569a3da9fb6e886b60c876b4a186c6
Marcus Eaton <marcus.eaton@crisisresponseunit.com>
2015-06-25 11:01:36 -0700

    Added > NUL to clean up output

--- update-spice-tools.bat
+++ update-spice-tools.bat
@@ -1,5 +1,5 @@
 echo I- Downloading spice-guest-tools-0.100.exe - 5MB
 echo.
-bin\wget.exe -q --no-check-certificate http://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-0.100.exe
+bin\wget.exe -q --no-check-certificate http://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-0.100.exe > NUL
 echo I- Moving spice-guest-tools-0.100.exe to multiarch folder
 move spice-guest-tools-0.100.exe multiarch
